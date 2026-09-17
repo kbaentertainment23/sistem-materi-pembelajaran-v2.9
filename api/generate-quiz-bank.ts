8aa5fbcc492f342cbda3ffed659e111acb8af704
kbaentertainment23 <kbaentertainment23@gmail.com>
2026-09-17 23:48:40 +0800

    Update generate-quiz-bank.ts

--- api/generate-quiz-bank.ts
+++ api/generate-quiz-bank.ts
@@ -1,5 +1,5 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
-import { generateQuizBankQuestionsWithAI } from './_lib/quizGeneratorCore';
+import { generateQuizBankQuestionsWithAI } from './_lib/quizGeneratorCore.js';
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   res.setHeader('Access-Control-Allow-Origin', '*');
